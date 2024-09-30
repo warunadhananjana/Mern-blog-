9ce895b67e8a83592cc9a60f19edf2baf3621560
dhananjana123 <warunad999@gmail.com>
2024-09-30 17:50:09 +0530

    add style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,15 +3,16 @@ import {Link} from "react-router-dom"
 import Logo from '../image/OSKHFO0.jpg'
 import { FaBars } from "react-icons/fa6";
 // import {AiOutlineClone} from "react-icons/ai"
+
 const Header = () => {
   return (
     <nav>
       <div className='container nav_container'>
-        <Link to="/" className='nav_logo'>
-            <img src={Logo} alt="Navbar Logo"/>
+        <Link to="/" >
+            <img className='nav_logo' src={Logo} alt="Navbar Logo"/>
         </Link>
             <ul className='nav_menu'>
-              <li><Link to="/profile">Ernest Achiever</Link></li>
+              <li><Link to="/profile/:id">Ernest Achiever</Link></li>
               <li><Link to="/create">Crete post</Link></li>
               <li><Link to="/authors">Authors</Link></li>
               <li><Link to="/logout">Logout</Link></li>
